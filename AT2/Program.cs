9fae1190f58f62fa67136b66fd91f00e837dc784
Doherty <gabriel.dcarvalho@al.infnet.edu.br>
2024-09-24 20:16:11 -0300

    Adicionar arquivos de projeto.

---
+++ AT2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AT2
+{
+    public delegate string ImprimirMensagem(string mensagem);
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirMensagem imprimirMaiusculo = delegate (string mensagem)
+            {
+                return mensagem.ToUpper();
+            };
+
+            ImprimirMensagem imprimirMinusculo = delegate (string mensagem)
+            {
+                return mensagem.ToLower();
+            };
+
+            Console.WriteLine("Digite uma frase:");
+            string mensagem = Console.ReadLine();
+
+            Console.WriteLine(imprimirMaiusculo(mensagem));
+            Console.WriteLine(imprimirMinusculo(mensagem));
+        }
+    }
+}
